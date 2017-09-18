5b203eb38bded8b2a2c880233eb1f7f815b25239
Sam Mousa <sam@mousa.nl>
2017-09-18 12:15:19 +0200

    Removed unneeded phpdoc

--- src/Behaviors/JsonBehavior.php
+++ src/Behaviors/JsonBehavior.php
@@ -38,13 +38,6 @@ class JsonBehavior extends Behavior
 
     }
 
-    /**
-     * Attaches the behavior object to the component.
-     * The default implementation will set the [[owner]] property
-     * and attach event handlers as declared in [[events]].
-     * Make sure you call the parent implementation if you override this method.
-     * @param Component $owner the component that this behavior is to be attached to.
-     */
     public function attach($owner)
     {
         parent::attach($owner);
